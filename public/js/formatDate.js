1466a778908c1651f56cc0a0af30644c5a88e052
RazorPG <rafidhilmi0504@gmail.com>
2025-02-23 13:00:33 +0700

    merubah sedikit logic yang ada di public/js/formatDate.js dengan mengambil berdasarkan value element bukan value atribute

--- public/js/formatDate.js
+++ public/js/formatDate.js
@@ -2,17 +2,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const dateElements = document.querySelectorAll('.post-date')
   dateElements.forEach(el => {
-    // Asumsikan tanggal mentah disimpan dalam attribute data-date
-    const rawDate = el.getAttribute('data-date')
-    if (rawDate) {
-      const date = new Date(rawDate)
-      const formatter = new Intl.DateTimeFormat('en-US', {
-        weekday: 'long',
-        month: 'short',
-        day: '2-digit',
-        year: 'numeric',
-      })
-      el.textContent = formatter.format(date)
-    }
+    const date = new Date(el.textContent.trim())
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      weekday: 'long',
+      month: 'short',
+      day: '2-digit',
+      year: 'numeric',
+    })
+    el.textContent = formatter.format(date)
   })
 })
