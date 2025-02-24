0dea95582c373b1d6e31a23451e0cd48ec745943
RazorPG <rafidhilmi0504@gmail.com>
2025-02-24 10:41:06 +0700

    memperbaiki middware validateSchema agar ketika error balik ke tempat semua

--- middlewares/validateSchema.js
+++ middlewares/validateSchema.js
@@ -1,10 +1,12 @@
+const { ErrorHandler } = require('../utils/ErrorHandler')
+
 module.exports = schema => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body)
     if (error) {
       const message = error.details.map(msg => msg.message).join(',')
       req.flash('error', message)
-      res.redirect('/register')
+      return res.redirect(req.originalUrl)
     }
     return next()
   }
